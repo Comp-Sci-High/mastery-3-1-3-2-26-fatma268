704ae7769b49251f1a54685642bd434e9387a685
fatma268 <fatma.diagne26@compscihigh.org>
2026-02-26 13:02:38 +0000

    commit

--- index.js
+++ index.js
@@ -4,6 +4,11 @@
 
 // Step 2: Create an instance of an Express application
 
+const express = require("express")
+const app = express()
 // Step 3: Start the server on port 3000
 
+app.listen(3000, () => {
+    console.log("Server is running")
+})
 // Step 4: Run this file, use the command: `node index.js`
